d1ae466ce1864f4db1653792590dc33026adb84b
Kazi Rifat Al Muin <muin2107042@stud.kuet.ac.bd>
2025-12-18 10:42:27 +0600

    ICPC Dhaka Regional 2025

--- code/Misc/Bit Hacks.cpp
+++ code/Misc/Bit Hacks.cpp
@@ -27,6 +27,13 @@ a - b = (a ^ (a & b)) - ((a | b) ^ a)
 a - b = ((a | b) ^ b) - ((a | b) ^ a)
 a - b = (a ^ (a & b)) - (b ^ (a & b))
 a - b = ((a | b) ^ b) - (b ^ (a & b))
+// Precompute XOR from 1 to n
+ll prexor(ll n){
+    if(n % 4 == 0) return n;
+    else if(n % 4 == 1) return 1;
+    else if(n % 4 == 2) return n + 1;
+    else return 0;
+}
 // Bit Hacks
 # x & -x is the least bit in x.
 # iterate over all the subsets of the mask
